b1ea57b1aacd73df04770584d9ff6205faae8c4f
JongInHey <youyi1234@naver.com>
2024-09-03 17:40:08 +0900

    backbtn setup

--- src/pages/note/Note.js
+++ src/pages/note/Note.js
@@ -1,12 +1,31 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container } from "@chakra-ui/react";
 import { PageTitle } from "../../components/PageTitle";
 import { Todo } from "./components/Todo";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 
 export const Note = () => {
+  const navi = useNavigate();
   return (
     <>
       <PageTitle titleName={"Note"} />
-      <Container maxW="450px" w="100%" minH="100vh" bg="#f4f3ee" p="80px 10px">
+      <Container
+        maxW="450px"
+        w="100%"
+        minH="100vh"
+        bg="#f4f3ee"
+        p="80px 10px"
+        pos="relative"
+      >
+        <Box
+          onClick={() => navi(-1)}
+          cursor="pointer"
+          pos="absolute"
+          left="3%"
+          top="10%"
+        >
+          <ChevronLeftIcon fontSize="26px" />
+        </Box>
         <Todo />
       </Container>
     </>
